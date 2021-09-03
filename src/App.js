2db233a583bba99aa47d09f40c993bae19da9750
karlaevelize <k.evelize@gmail.com>
2021-09-03 15:58:49 +0200

    react introduction

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from "./component/Button"
 
 function App() {
+
+  const students = [
+    {
+      name: "Sara"
+    }, 
+    {
+      name: "Anne"
+    },
+    {
+      name: "Kamil"
+    },
+    {
+      name: "Mariel"
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, React!</h1>
+      <Button/>
+      {students.map(student => {
+        return <div>
+          <p>{student.name}</p>
+          <Button/>
+          </div>
+      })}
     </div>
   );
 }
---
+++ src/component/Button.js
@@ -0,0 +1,6 @@
+export default function Button (){
+
+  return (
+    <button onClick={() => console.log("clicked")}>Click me</button>
+  )
+}
